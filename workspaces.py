f4a304e708ac51e8cfafe5371408d4400ba2c873
dschmaryl <dschmaryl@gmail.com>
2018-02-06 18:15:51 -0500

    refactor a bit

--- workspaces.py
+++ workspaces.py
@@ -5,23 +5,31 @@ from json import loads
 from subprocess import check_output
 
 
-# use hex codes here or the color variables from conkyrc
+# use hex codes or the color variables from conkyrc
 COLOR_FOCUSED = 'color8'
 COLOR_UNFOCUSED = 'color4'
 COLOR_INVISIBLE = 'color1'
 
-COMMAND = 'i3-msg -t get_workspaces'
+WORKSPACES_COMMAND = 'i3-msg -t get_workspaces'
 
-spaces = loads(check_output(COMMAND.split()).decode('utf-8'))
-active_spaces = {space['num']: space['focused'] for space in spaces}
-output = []
 
-for i in range(1, 11):
-    workspace_num = '0' if i == 10 else str(i)
-    try:
-        color = COLOR_FOCUSED if active_spaces[i] else COLOR_UNFOCUSED
-        output.append('${' + color + '}' + workspace_num)
-    except KeyError:
-        output.append('${' + COLOR_INVISIBLE + '}' + workspace_num)
+def get_visible_workspaces():
+    # returns a dict pairing the visible workspaces with the focused state of
+    # each of these workspaces
+    visible_spaces = loads(check_output(WORKSPACES_COMMAND.split()))
+    return {space['num']: space['focused'] for space in visible_spaces}
 
-print(' '.join(output))
+
+def format_for_conky(visible_spaces):
+    output = []
+    for i in range(1, 11):
+        try:
+            color = COLOR_FOCUSED if visible_spaces[i] else COLOR_UNFOCUSED
+            output.append('${%s}%i' % (color, i))
+        except KeyError:
+            output.append('${%s}%i' % (COLOR_INVISIBLE, i))
+    return ' '.join(output)
+
+
+if __name__ == '__main__':
+    print(format_for_conky(get_visible_workspaces()))
